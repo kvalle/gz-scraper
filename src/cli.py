285d733a5c98ecbfc8b144906fb3364aa75fbdc8
Kjetil Valle <kjetil.valle@gmail.com>
2020-02-18 15:19:05 +0100

    list backlog using cli

--- src/cli.py
+++ src/cli.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python
 
+import dynamodb
+import argparse
 import lambda_function
 import os
 import sys
+
+def print_backlog():
+    print("Backlog:\n")
+
+    for item in dynamodb.get_backlog():
+        name = item["game_name"]
+        excludes = ", ".join(item["exclude_keywords"])
+        print('- "{}" (excluded keywords: {})'.format(name, excludes))
 
 if __name__ == "__main__":
 
@@ -14,4 +24,16 @@ if __name__ == "__main__":
         print("Looks like you forgot AWS credentials…")
         sys.exit(1)
 
-    lambda_function.run_scraper()
+    parser = argparse.ArgumentParser(prog='gz-scaper')
+    subparsers = parser.add_subparsers(dest="subcommand", metavar="subcomands", required=True)
+
+    parser_scrape = subparsers.add_parser('scrape', help='trigger the scraper locally')
+    parser_list = subparsers.add_parser('list', help='display backlog items')
+
+    args = parser.parse_args()
+
+    if args.subcommand == "scrape":
+        print("Running scraper.")
+        lambda_function.run_scraper()
+    elif args.subcommand == "list":
+        print_backlog()
